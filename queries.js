6e5e3fb07ef503e398502fb1361206c8bb4b0eda
Juan <juanssal@gmail.com>
2022-06-20 19:09:23 +0200

    Add dotenv config

--- queries.js
+++ queries.js
@@ -1,13 +1,14 @@
 const { append } = require("express/lib/response");
+require('dotenv').config()
 
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "crm_radios",
-  password: "Horchata3!",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // COMMENTS
